93944b3868bf8b1acaef1581e8664a35168cc462
Duy Huynh <d.huynh68@gmail.com>
2015-06-07 14:28:02 -0400

    Adding Monthly Performance for DH Apparel :)

--- src/public/js/revenue.js
+++ src/public/js/revenue.js
@@ -1,27 +1,28 @@
 d3.json('/data/revenue.json', function(data) {
   nv.addGraph(function() {
-    var graph = nv.models.cumulativeLineChart()
+    var chart = nv.models.cumulativeLineChart()
                   .x(function(d) { return d[0] })
-                  .y(function(d) { return d[1]/100 }) //adjusting, 100% is 1.00, not 100 as it is in the data
+                  .y(function(d) { return d[1]*100 }) //Keeping values low, 1.0 = 100
                   .color(d3.scale.category10().range())
-                  .useInteractiveGuideline(true);
+                  .useInteractiveGuideline(true)
+                  ;
 
-     graph.xAxis
-        .tickValues([1078030800000,1122782400000,1167541200000,1251691200000])
+     chart.xAxis
+        .tickValues([1401595200000, 1404187200000, 1406865600000, 1409544000000, 1412136000000, 1414814400000, 1417410000000, 1420088400000, 1422766800000, 1425186000000, 1427860800000, 1430452800000, 1433131200000])
         .tickFormat(function(d) {
-            return d3.time.format('%x')(new Date(d))
+            return d3.time.format('%b-%Y')(new Date(d))
           });
 
-    graph.yAxis
-        .tickFormat(d3.format(',.1%'));
+    chart.yAxis
+        .tickFormat(d3.format('$,f'));
 
     d3.select('#chart2 svg')
         .datum(data)
-        .call(graph);
+        .call(chart);
 
     //TODO: Figure out a good way to do this automatically
-    nv.utils.windowResize(graph.update);
+    nv.utils.windowResize(chart.update);
 
-    return graph;
+    return chart;
   });
 });
